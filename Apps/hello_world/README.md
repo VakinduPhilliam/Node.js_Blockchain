a46335b300fd57e0ca8eae9c0a94a643c4eefeb9
Vakindu Philliam <Pvakindu@gmail.com>
2019-09-01 20:40:16 +0300

    Add files via upload

---
+++ Apps/hello_world/README.md
@@ -0,0 +1,34 @@
+## Hello World
+ App with Lisk SDK
+
+This is the reference implementation for the Hello World example, find the full step by step guide: 
+(https://github.com/LiskHQ/lisk-sdk-examples/tree/RachBLondon-patch-1/hello_world).
+
+
+Post a custom transaction to /api/transactions:
+
+
+
+
+{  
+   
+
+"id":"1199714748623931346",
+   "amount":"0",
+   "type":10,
+   "timestamp":0,
+   
+"senderPublicKey":"c094ebee7ec0c50ebee32918655e089f6e1a604b83bcaa760293c61e0f18ab6f",
+   
+"senderId":"16313739661670634666L",
+   "recipientId":"10881167371402274308L",
+   "fee":"100000000",
+   
+"signature":"e6da5923ee9b769bd5624612af536ca4348d5b32c4552a05161a356e472b8708487022fd4e9787a1b7e548a98c64341f52f2b8b12a39d4115f820b8f01064003",
+   
+"signatures":[],
+   "asset":{  
+      "hello":"world"
+   }
+}
+```
---
+++ Apps/hello_world/hello_transaction.js
@@ -0,0 +1,157 @@
+const {
+ transactions: { BaseTransaction, TransactionError },
+} = require('lisk-sdk');
+
+
+
+class HelloTransaction extends BaseTransaction {
+
+	
+
+static get TYPE () {
+		
+
+return 10;
+	
+
+};
+
+	
+
+static get FEE () {
+		
+
+return `${10 ** 8}`;
+	
+
+};
+
+	
+
+applyAsset(store) {
+        
+
+const errors = [];
+        
+const sender = store.account.get(this.senderId);
+        
+
+if (sender.asset && sender.asset.hello) {
+            
+
+errors.push(
+ new TransactionError(
+'You cannot send a hello transaction multiple times',
+ this.id,
+ '.asset.hello',
+ this.amount.toString()
+ )
+ );
+        
+
+} else {
+            
+
+const newObj = { ...sender, asset: { hello: this.asset.hello } };
+            
+
+store.account.set(sender.address, newObj);
+        
+
+}
+        
+
+return errors; // array of TransactionErrors, returns empty array if no errors are thrown
+	
+
+}
+
+	
+
+undoAsset(store) {
+		
+
+const sender = store.account.get(this.senderId);
+		
+const oldObj = { ...sender, asset: null };
+		
+
+store.account.set(sender.address, oldObj);
+		
+
+return [];
+	
+
+}
+
+	
+
+validateAsset() {
+		
+
+const errors = [];
+		
+
+if (!this.asset.hello || typeof this.asset.hello !== 'string' || this.asset.hello.length > 64) {
+			
+
+errors.push(
+				
+
+new TransactionError(
+
+
+'Invalid "asset.hello" defined on transaction',
+
+ this.id,
+
+ '.asset.hello',
+ this.asset.hello,
+	
+'A string value no longer than 64 characters',
+				
+
+)
+			
+
+);
+		
+
+}
+		
+
+return errors;
+	
+
+}
+	
+
+// Server
+	
+
+async prepare(store) {
+		
+
+await store.account.cache([
+			
+
+{
+				
+
+address: this.senderId,
+			
+
+},
+		
+]);
+	
+
+}
+
+
+}
+
+
+
+module.exports = HelloTransaction;
+
